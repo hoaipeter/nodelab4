0168a36b84bdf76d4bf966086ae86b6dc274d737
rgonvp1 <rgonvp1@hotmail.com>
2018-05-05 12:17:11 -0500

    Revert "Adjustment to reuse findById code for all methods that require it"
    This reverts commit fb8977c8fa77b7f2f583263f3b2a56ee8dc948b6.

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,6 @@ router.get('/accounts/:id', accounts.getAccounts)
 router.post('/accounts', accounts.addAccount)
 router.put('/accounts/:id', accounts.updateAccount)
 router.delete('/accounts/:id',accounts.removeAccount)
-router.param('id',accounts.idParam)
 
 
 module.exports = router
